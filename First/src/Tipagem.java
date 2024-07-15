f36b8eefdf47826eb989b3f0a9646705fd90e0a0
Diogo Feld <cfeld.diogo@gmail.com>
2024-07-14 21:56:00 -0300

    Added Tipagem

---
+++ First/src/Tipagem.java
@@ -0,0 +1,35 @@
+public class Tipagem {
+
+    public static void main(String[] args) {
+
+        final double piValue = 3.14;
+        int raio = 4;
+
+        System.out.println(piValue * raio * raio);
+
+        byte menor = (byte) 140;
+        System.out.println(menor);
+
+        short pequeno = 2000;
+        System.out.println(pequeno);
+
+        int normal = 416548;
+        System.out.println(normal);
+
+
+        long grande = 2165496464L;
+        System.out.println(grande);
+
+
+        char letra = 'A';
+        System.out.println(letra);
+
+    }
+
+
+
+
+
+
+
+}
